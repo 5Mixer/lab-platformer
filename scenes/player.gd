3fe6dae0f406c86b5e759fd9139384cdda9d78f8
Daniel B <dantheman41@live.com.au>
2021-01-17 18:21:53 +1100

    Fixed collisions, mesh of meshlib must be parent

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,26 +1,30 @@
 extends KinematicBody
 
-export var gravity = -30
+export var gravity = -.5
 export var speed = 10
 export var turn_speed = .05
+export var jump_velocity = 15
 
-func get_input_movement():
-	var direction = Vector3()
-	
-	if Input.is_action_pressed("forward"):
-		direction += global_transform.basis.z
-	if Input.is_action_pressed("back"):
-		direction -= global_transform.basis.z
-	if Input.is_action_pressed("left"):
-		rotate_y(turn_speed)
-	if Input.is_action_pressed("right"):
-		rotate_y(-turn_speed)
-	
-	# Normalise so that moving diagonally is not faster.
-	direction = direction.normalized()
-	return direction
+var velocity = Vector3()
 
 func _physics_process(delta):
-	var movement_velocity = get_input_movement() * speed
+	var input_velocity = Vector3()
+	if Input.is_action_pressed("forward"):
+		input_velocity += global_transform.basis.z * speed
+	if Input.is_action_pressed("back"):
+		input_velocity -= global_transform.basis.z * speed
 	
-	move_and_slide(movement_velocity, Vector3.UP)
+	velocity.x = input_velocity.x
+	velocity.z = input_velocity.z
+		
+	if Input.is_action_pressed("left"):
+		rotate_y(-turn_speed)
+	if Input.is_action_pressed("right"):
+		rotate_y(turn_speed)
+		
+	velocity.y += gravity
+	
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+	
+	velocity = move_and_slide(velocity, Vector3.UP)
