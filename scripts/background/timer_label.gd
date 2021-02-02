9ad9ce8cfdac17fa65c1a55c25dc5ce23358be4d
Daniel B <dantheman41@live.com.au>
2021-02-02 15:02:27 +1100

    Add reduction variable to timer

--- scripts/background/timer_label.gd
+++ scripts/background/timer_label.gd
@@ -2,13 +2,13 @@ extends Label
 
 export var count_upwards = true
 export var current_time = 0.0
+export var collectable_timer_reduction = 3.0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 func on_collect():
-	current_time = max(current_time - 5, 0)
+	current_time = max(current_time - collectable_timer_reduction, 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
