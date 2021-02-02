d6602089ee339b7b1acba9b4c2ba659f66117d57
Daniel B <dantheman41@live.com.au>
2021-02-02 13:39:18 +1100

    Add sound effects for launching and collecting

--- scripts/collectable.gd
+++ scripts/collectable.gd
@@ -3,6 +3,8 @@ extends Area
 func _ready():
 	connect("body_entered", self, "on_collect")
 
-func on_collect(_body):
+func on_collect(body):
+	if body.has_method("collect"):
+		body.collect()
 	get_tree().get_root().get_node("game_root").on_collection()
 	get_node("../../../").queue_free()
